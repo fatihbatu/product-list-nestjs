92a94deecf7a684c6a427a9a3c1746cef68adcdc
fatihbatu <77248966+fatihbatu@users.noreply.github.com>
2024-03-21 01:39:05 +0300

    refactor: code optimized and revised for products controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
+
+type PinProductDto = {
+  position: number;
+};
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Post('generate')
+  @Post('seed')
   generateMock() {
     return this.productsService.generateMock();
   }
@@ -13,5 +17,18 @@ export class ProductsController {
   @Get()
   findAll(@Query('sort') sort?: 'price' | 'name' | 'id') {
     return this.productsService.findAll(sort);
+  }
+
+  @Put('pin/:productId')
+  async pinProduct(
+    @Param('productId') productId: string,
+    @Body() pinProductDto: PinProductDto,
+  ) {
+    return this.productsService.pinProduct(+productId, pinProductDto);
+  }
+
+  @Put('unpin/:productId')
+  async unpinProduct(@Param('productId') productId: string) {
+    return this.productsService.unpinProduct(+productId);
   }
 }
