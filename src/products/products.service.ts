514e3a382f866f7e1181e6defcb9aa6a51c7a909
fatihbatu <77248966+fatihbatu@users.noreply.github.com>
2024-03-21 01:39:59 +0300

    feat: logics implemented for findAll,pinProduct,unpinProduct

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 import * as fs from 'fs';
 import * as path from 'path';
+
+type PinProductDto = {
+  position: number;
+};
 
 @Injectable()
 export class ProductsService {
@@ -16,14 +25,103 @@ export class ProductsService {
       data: products,
     });
 
-    return 'Mock data generated';
+    return 'Seed successfully completed';
   }
 
   async findAll(sort?: 'price' | 'name' | 'id') {
-    return this.databaseService.product.findMany({
+    let products = await this.databaseService.product.findMany({
       orderBy: {
         [sort || 'id']: 'asc',
       },
     });
+
+    if (!products.length) {
+      throw new NotFoundException('No products found');
+    }
+
+    const pinnedProducts = [...products]
+      .filter((product) => product.isPinned)
+      .sort((a, b) => a.pinPosition - b.pinPosition);
+
+    const unpinnedProducts = [...products].filter(
+      (product) => !product.isPinned,
+    );
+
+    pinnedProducts.forEach((pinnedProduct) => {
+      console.log('pinnedProduct', pinnedProduct);
+      unpinnedProducts.splice(pinnedProduct.pinPosition - 1, 0, pinnedProduct);
+    });
+
+    return unpinnedProducts;
+  }
+
+  async pinProduct(productId: number, pinProductDto: PinProductDto) {
+    if (!pinProductDto.position || pinProductDto.position < 1) {
+      throw new BadRequestException('Missing required field');
+    }
+
+    const product = await this.databaseService.product.findUnique({
+      where: {
+        id: productId,
+      },
+    });
+
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+
+    if (pinProductDto.position < 1) {
+      throw new BadRequestException();
+    }
+
+    const positionCheck = await this.databaseService.product.findFirst({
+      where: {
+        isPinned: true,
+        pinPosition: pinProductDto.position,
+      },
+    });
+
+    if (positionCheck) {
+      throw new ConflictException('Position already taken');
+    }
+
+    const record = this.databaseService.product.update({
+      where: {
+        id: productId,
+      },
+      data: {
+        isPinned: true,
+        pinPosition: pinProductDto.position,
+      },
+    });
+
+    return record;
+  }
+
+  async unpinProduct(productId: number) {
+    const record = await this.databaseService.product.findFirst({
+      where: {
+        id: productId,
+        isPinned: true,
+      },
+    });
+
+    if (!record) {
+      throw new NotFoundException('Product not pinned');
+    }
+
+    const unpinnedRecord = await this.databaseService.product.update({
+      where: {
+        id: productId,
+      },
+      data: {
+        isPinned: false,
+        pinPosition: null,
+      },
+    });
+
+    console.log(unpinnedRecord);
+
+    return unpinnedRecord;
   }
 }
