cb31f74141c704128a4f04beba4fb9e906462339
fatihbatu <77248966+fatihbatu@users.noreply.github.com>
2024-03-25 00:03:54 +0300

    fix: Toaster location change

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,6 @@
-import { Toaster } from 'react-hot-toast';
-
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center min-h-screen py-2 shadow-sm m-20 rounded-lg bg-slate-500">
-      <Toaster toastOptions={{ position: 'top-center' }} />
       <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1>Product Demo App</h1>
         <p>List of products</p>
