59cced9ff6fea362536771acb8b47c7924d5b789
fatihbatu <77248966+fatihbatu@users.noreply.github.com>
2024-03-25 15:53:04 +0300

    style: style changed

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,25 +9,17 @@ export default function Home() {
   const { trigger: seedTrigger } = useSeedProduct();
   return (
     <section className="flex flex-col items-center justify-start h-screen w-full shadow-sm bg-white p-16">
-      <div
-        style={{
-          flex: 1,
-        }}
-        className="flex flex-col justify-center w-full h-full"
-      >
+      <div className="flex flex-col justify-center w-full h-full">
         <h1 className="text-4xl font-bold text-center text-black">
-          Welcome to the Product Demo App
+          Welcome to
+        </h1>
+        <h1 className="text-6xl font-bold text-center text-black">
+          Product Demo App
         </h1>
       </div>
-      <div
-        style={{
-          flex: 1,
-        }}
-        className="flex flex-col justify-center w-full h-full"
-      >
+      <div className="flex flex-col justify-center w-full h-full">
         <div className="flex flex-row justify-center w-full h-full space-x-4">
           <Button
-            className="bg-[#03045e] hover:bg-[#02098a] text-white font-bold p-16 rounded-md w-full h-full"
             onClick={() => seedTrigger()}
             disabled={data && data.length > 0}
           >
@@ -35,10 +27,8 @@ export default function Home() {
               ? 'Database Seeded Successfully!'
               : 'Seed Database'}
           </Button>
-          <a className="w-full h-full" href="/products">
-            <Button className="bg-[#03045e] hover:bg-[#02098a] text-white font-bold p-16 rounded-md w-full h-full">
-              Go to Products
-            </Button>
+          <a className="" href="/products">
+            <Button>Go to Products</Button>
           </a>
         </div>
       </div>
