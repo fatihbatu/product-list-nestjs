e6628dec9d6649352d4943fab2743a3cebaa2f59
fatihbatu <77248966+fatihbatu@users.noreply.github.com>
2024-03-28 04:45:57 +0300

    refactor: remove swr files

--- frontend/app/services/queries.ts
+++
@@ -1,9 +0,0 @@
-import useSWR from 'swr';
-import { Product } from '@/app/types';
-
-export function useProducts() {
-  return useSWR<Product[]>('/products');
-}
-export function useSortedProducts() {
-  return useSWR<Product[]>('/products?sort=price');
-}
